ea4322a233007e99bccf268a83b7eb30acba5aed
Alisa Morenko <Alisa.Morenko@gmail.com>
2021-05-17 18:49:19 +0300

    4

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './sass/main.scss';
+import countryCardTmpl from './templates/countryCard.hbs';
+import countryListTmpl from './templates/countryList.hbs';
+
+const refs = {
+  cardContainer: document.querySelector('.js-card-container'),
+};
+
+fetchCountry().then(renderCountryCard);
+// .catch (error => {
+//     console.log(error);
+// });
+
+function fetchCountry() {
+  return fetch('https://restcountries.eu/rest/v2/name/afganistan').then(response => {
+    return response.json;
+  });
+}
+
+function renderCountryCard(a) {
+  const markUp = countryCardTmpl(a);
+  refs.cardContainer.innerHTML = markUp;
+}
